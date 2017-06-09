67e85713ed6a8369ca9d6e774fc90cf758dfadde
SushantPatekar <patesush@gmail.com>
2017-06-09 18:19:59 +0530

    Initial commit

---
+++ app/src/main/java/com/firebase/sample/demo/test/Item.java
@@ -0,0 +1,23 @@
+package com.firebase.sample.demo.test;
+
+/**
+ * Created by echessa on 8/27/16.
+ */
+public class Item {
+
+    private String title;
+
+    public Item() {}
+
+    public Item(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
